0ccb1543c921c6a68174daf4319abb60a73a9e2a
Sayyadsulthan <asulthan088@gmail.com>
2023-09-18 10:47:00 +0530

    initialized app, add  component and page folder

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+// import logo from './logo.svg';
+import "../styles/App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>App</h1>
+    </div>
+  );
+}
+
+export default App;
